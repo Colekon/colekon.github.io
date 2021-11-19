1af87ce43dc074cff7d95b7f04576acc20b3e10a
Alyssa Donchu <55606256+AlyssaDonchu@users.noreply.github.com>
2021-11-19 11:18:40 -0800

    email connect + form bug fix

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-const scaleFactor = 1 / 20;
+/* 
 
-function moveBackground(event) {
-  const shapes = document.querySelectorAll(".shape");
-  const x = event.clientX * scaleFactor;
-  const y = event.clientY * scaleFactor;
+   ------------------------------- SEND EMAIL -------------------------------
 
-  console.log(x, y);
-
-  for (let i = 0; i < shapes.length; ++i) {
-    const isOdd = i % 2 !== 0;
-    const oddInteger = isOdd ? -1 : 1;
-    shapes[i].style.transform = `translate(${x * oddInteger}px, ${
-      y * oddInteger
-    }px)`;
-  }
-}
+*/
 
 function contact(event) {
   event.preventDefault();
-
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
-  setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible");
-    success.classList += " modal__overlay--visible";
-    console.log("it worked 1");
-  }, 1000);
+  emailjs
+    .sendForm(
+      "colekonmusic@gmail.com",
+      "template_e7nrxbc",
+      event.target,
+      "user_ew2b5AYyNH8FSnXLgF629"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please, contact me directly on Instagram or email: colekonmusic@gmail.com"
+      );
+    });
 }
+
+/* 
+
+   ------------------------------- MODAL -------------------------------
+
+*/
 
 let isModalopen = false;
 function toggleModal() {
@@ -37,4 +42,26 @@ function toggleModal() {
   }
   isModalopen = !isModalopen;
   document.body.classList += " modal--open";
+}
+
+/* 
+
+   ------------------------------- SHAPES -------------------------------
+
+*/
+
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const oddInteger = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * oddInteger}px, ${
+      y * oddInteger
+    }px)`;
+  }
 }
